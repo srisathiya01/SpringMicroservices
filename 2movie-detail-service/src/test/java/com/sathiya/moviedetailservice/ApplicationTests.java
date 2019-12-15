3ac2e5bc7a0e05befec12284498d0986259577f3
VENKATACHALAPATHY Sathiyaraj <sathiyaraj.venkatachalapathy@rntbci.com>
2019-12-15 10:09:33 +0530

    first commit

---
+++ 2movie-detail-service/src/test/java/com/sathiya/moviedetailservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathiya.moviedetailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
